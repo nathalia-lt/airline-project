c5713975cb2b17310912db86977cd2e0e4525a2f
nathalia <nathaliatroina@gmail.com>
2024-11-08 18:46:09 -0500

    first commit

---
+++ airline/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Header from './components/Header/Header'
+
+function App() {
+  const test = "a"
+
+  return (
+    <>
+      <div>
+        <Header/>
+        <h1>Airline {test}</h1>
+      </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ airline/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import './Header.css'
+
+function Header (){
+
+
+
+
+
+
+    return (
+        <header>Book your Flight</header>
+    )
+}
+
+
+export default Header
