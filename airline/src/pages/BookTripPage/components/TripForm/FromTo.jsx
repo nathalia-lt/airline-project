ee0250e64a634677879394eeff3e878625e7fd43
nathalia <nathaliatroina@gmail.com>
2024-11-26 15:40:28 -0500

    fromtocard

--- airline/src/pages/BookTripPage/components/TripForm/FromTo.jsx
+++ airline/src/pages/BookTripPage/components/TripForm/FromTo.jsx
@@ -4,14 +4,14 @@ function FromTo(){
 
     return(
         <div className="from-to-container">
-        <fieldset className="from-to-box">
+        <fieldset className="from-to-card">
             <div className="input-wrappper">
                 <label htmlFor="">From</label>
                 <input type="text" placeholder="Select City"/>
             </div>
         </fieldset>
         <button>slt</button>
-        <fieldset className="from-to-box">
+        <fieldset className="from-to-card">
             <div className="input-wrappper">
                 <label htmlFor="">TO</label>
                 <input type="text" placeholder="Select City"/>
